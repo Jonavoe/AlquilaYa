5ff29a26c23ef75e57c024909cb85db0b900381e
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-05-31 23:58:43 -0300

    Create RentsRouter.js

---
+++ api/src/routes/RentsRouter.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const {
+  postRentsHandler,
+  getAllRentsHandler,
+  getRentsByEmailHandler,
+  getRentsByIdHandler,
+  updateRentsHandler,
+  putRentsLeadHandler,
+} = require("../Handlers/RentsHandlers");
+
+const RentsRouter = Router();
+
+RentsRouter.post("/", postRentsHandler);
+RentsRouter.get("/", getAllRentsHandler);
+RentsRouter.get("/email", getRentsByEmailHandler);
+RentsRouter.get("/:id", getRentsByIdHandler);
+RentsRouter.put("/:id", updateRentsHandler);
+
+module.exports = RentsRouter;
