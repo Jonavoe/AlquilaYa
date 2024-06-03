2792b3c7f63be10b8d3ec95cbb5bbbb25c186ad4
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-06-03 15:12:51 -0300

    Update Rents.js

--- api/src/models/Rents.js
+++ api/src/models/Rents.js
@@ -1,66 +1,58 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const RentsSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: validator.isEmail,
-        message: "El correo electrónico debe tener un formato válido",
-      },
-    },
-    birthdate: {
-      type: String,
-    },
-    photo: {
-      type: String,
-    },
-    country: {
-      type: String,
-    },
-    contactNumber: {
-      type: String,
-    },
-    description: {
-      type: String,
-    },
-    classifications: {
-      type: String,
-    },
-    average_delay: {
-      type: String,
-    },
-    incidences: {
-      type: String,
-    },
-    hired_leads: {
-      type: String,
-    },
-    rol: {
-      type: String,
-      required: true,
-    },
-    leads: {
-      type: Array,
-      required: true,
-      default: [],
-    },
-    deleted: {
-      type: Boolean,
-      default: false,
-    },
+const RentsSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  provincia: {
+    type: String,
+    // required: [true, "La provincia es obligatoria"],
   },
-  { timestamps: true }
-);
+  ciudad: {
+    type: String,
+    // required: [true, "La ciudad es obligatoria"],
+  },
+  direccion: {
+    type: String,
+    // required: [true, "La dirección es obligatoria"],
+  },
+  tipoVivienda: {
+    type: String,
+    // required: [true, "El tipo de vivienda es obligatorio"],
+    // enum: ["departamento", "casa", "duplex"],
+  },
+  precio: {
+    type: Number,
+    // required: [true, "El precio es obligatorio"],
+    min: [0, "El precio no puede ser negativo"],
+  },
+  estadoInmueble: {
+    type: String,
+    // required: [true, "El estado del inmueble es obligatorio"],
+    // enum: ["disponible", "alquilado", "vendido", "en mantenimiento"], // Ajusta las opciones según tus necesidades
+  },
+  descripcion: {
+    type: String,
+    // required: [true, "La descripción es obligatoria"],
+  },
+  // fotos: {
+  //   type: [String],
+  //   validate: {
+  //     validator: (v) => v.every(validator.isURL),
+  //     message: 'Las fotos deben ser URLs válidas',
+  //   },
+  // },
+  clasificaciones: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Clasificaciones" },
+  ],
+  opiniones: [{ type: mongoose.Schema.Types.ObjectId, ref: "Opiniones" }],
+  mensajes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Mensajes" }],
+  idUsuarioPropietario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Usuarios",
+    // required: [true, "El ID del usuario propietario es obligatorio"],
+  },
+});
 
-
-const Rents = new mongoose.model("Rents", RentsSchema);
+const Rents = mongoose.model("Inmuebles", RentsSchema);
 
 module.exports = Rents;
