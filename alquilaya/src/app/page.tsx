efb969171ee4227d24a44ff6f4147dbbf7a12b83
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-06-03 20:49:50 -0300

    Update page.tsx

--- alquilaya/src/app/page.tsx
+++ alquilaya/src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between font-Poppins">
       <Header />
     </main>
   );
