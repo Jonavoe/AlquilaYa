39a6ad223435475962a685904fe597de0c77f697
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-06-03 20:49:47 -0300

    Update Header.tsx

--- alquilaya/src/components/Header/Header.tsx
+++ alquilaya/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from '../../assets/image/Logo.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -10,12 +11,12 @@ export default function Header({ }: Props) {
             <div className='h-full w-3/4 flex items-center justify-between'>
                 <div className=' flex gap-2 justify-center items-center'>
                     <Image src={Logo} alt='Logo' className='w-10 h-10' />
-                    <label htmlFor=""> AlquilaYa</label>
+                    <label htmlFor="" className='font-bold'> AlquilaYa</label>
                 </div>
-                <button className="btn btn-neutral btn-sm">Login</button>
-
+                <Link href={"/Login"}>
+                    <button className="btn btn-neutral btn-sm">Login</button>
+                </Link>
             </div>
-
         </div>
     )
 }
