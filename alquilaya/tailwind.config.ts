222c49ee478043a1807a2f063f97fbc764535e7d
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-06-04 14:04:22 -0300

    Update tailwind.config.ts

--- alquilaya/tailwind.config.ts
+++ alquilaya/tailwind.config.ts
@@ -7,13 +7,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primario: "#F8D02C",
+      text: {
+        primario: "#F8D02C",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily: { Poppins: "sans-serif" }, 
+      fontFamily: { Poppins: "sans-serif" },
     },
   },
   plugins: [require("daisyui")],
