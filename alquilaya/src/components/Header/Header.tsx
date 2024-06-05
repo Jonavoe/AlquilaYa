41e977513eaae42a6191ed08bd6166fcc9fc67ee
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-06-05 17:05:48 -0300

    Update Header.tsx

--- alquilaya/src/components/Header/Header.tsx
+++ alquilaya/src/components/Header/Header.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
+"use client"
 import Logo from '../../assets/image/Logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 
-type Props = {}
+interface Props {
+    user: { email: string } | null
+}
 
-export default function Header({ }: Props) {
+export default function Header({ user }: Props) {
     return (
         <div className='bg-gray-800 w-full h-16 flex justify-center'>
             <div className='h-full w-3/4 flex items-center justify-between'>
-                <div className=' flex gap-2 justify-center items-center'>
+                <div className='flex gap-2 justify-center items-center'>
                     <Image src={Logo} alt='Logo' className='w-10 h-10' />
-                    <label htmlFor="" className='font-bold'> AlquilaYa</label>
+                    <label htmlFor="" className='font-bold'>AlquilaYa</label>
                 </div>
-                <Link href={"/Login"}>
-                    <button className="btn btn-neutral btn-sm">Login</button>
-                </Link>
+                {user ? (
+                    <div>
+                        <label htmlFor="">{user.email}</label>
+                    </div>
+                ) : (
+                    <Link href="/Login">
+                        <button className="btn btn-neutral btn-sm">Login</button>
+                    </Link>
+                )}
             </div>
         </div>
     )
