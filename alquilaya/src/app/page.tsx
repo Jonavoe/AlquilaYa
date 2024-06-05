657c8fd5cd483dc0368042def99e1628b4c1695e
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-06-05 20:41:24 -0300

    Update page.tsx

--- alquilaya/src/app/page.tsx
+++ alquilaya/src/app/page.tsx
@@ -1,32 +1,16 @@
 "use client";
-import { useState, useEffect } from 'react';
 import Header from '@/components/Header/Header';
-import { useSearchParams } from 'next/navigation';
-import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { RootState } from '@reduxjs/toolkit/query';
 import Rents from '@/components/Rents/Rents';
 
-interface User {
-  email: string;
-}
 
 function Home() {
-  const [user, setUser] = useState<User | null>(null);
-  const searchParams = useSearchParams();
-  const email = searchParams.get('email');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (email) {
-        try {
-          const response = await axios.get<User>(`http://localhost:3001/api/users/email?email=${email}`);
-          setUser(response.data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
-    };
-    fetchData();
-  }, [email]);
+  const user = useSelector((state: RootState) => state.user);
+
+  console.log(user);
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between font-Poppins">
