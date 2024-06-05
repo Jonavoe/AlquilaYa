12869448283915b56398bea24aff2644636c8f65
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-06-05 17:05:46 -0300

    Update page.tsx

--- alquilaya/src/app/page.tsx
+++ alquilaya/src/app/page.tsx
@@ -1,9 +1,39 @@
-import Header from "@/components/Header/Header";
+"use client";
+import { useState, useEffect } from 'react';
+import Header from '@/components/Header/Header';
+import { useSearchParams } from 'next/navigation';
+import axios from 'axios';
+import Rents from '@/components/Rents/Rents';
 
-export default function Home() {
+interface User {
+  email: string;
+}
+
+function Home() {
+  const [user, setUser] = useState<User | null>(null);
+  const searchParams = useSearchParams();
+  const email = searchParams.get('email');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (email) {
+        try {
+          const response = await axios.get<User>(`http://localhost:3001/api/users/email?email=${email}`);
+          setUser(response.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+    };
+    fetchData();
+  }, [email]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between font-Poppins">
-      <Header />
+      <Header user={user} />
+      <Rents user={user} />
     </main>
   );
 }
+
+export default Home;
