471438b4b3d1b5db1132bb68e0d45ffa960b0ce3
JonathanVoeffray <voeffray.jonathan@gmail.com>
2024-06-06 16:37:58 -0300

    Update page.tsx

--- alquilaya/src/app/page.tsx
+++ alquilaya/src/app/page.tsx
@@ -1,16 +1,14 @@
 "use client";
-import Header from '@/components/Header/Header';
-import Rents from '@/components/Rents/Rents';
-import { useSelector } from 'react-redux';
-import { RootState } from '@reduxjs/toolkit/query';
-
+import Header from "@/components/Header/Header";
+import Rents from "@/components/Rents/Rents";
+import { useSelector } from "react-redux";
+import { RootState } from "@reduxjs/toolkit/query";
 
 function Home() {
-
   const user = useSelector((state: RootState) => state.user.user);
 
   return (
-    <main className="flex flex-col items-center justify-between font-Poppins">
+    <main className="flex flex-col items-center justify-between">
       <Header user={user} />
       <Rents user={user} />
     </main>
