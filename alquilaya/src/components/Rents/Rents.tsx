167fb3d349e880be812e7906c882063161fa4279
JonathanVoeffray <voeffray.jonathan@gmail.com>
2024-06-06 16:38:03 -0300

    Update Rents.tsx

--- alquilaya/src/components/Rents/Rents.tsx
+++ alquilaya/src/components/Rents/Rents.tsx
@@ -1,26 +1,86 @@
-import Image from 'next/image'
-import React from 'react'
-import Logo from "@/assets/image/Logo.png"
+import Image from "next/image";
+import React from "react";
+import Logo from "@/assets/image/Logo.png";
 
 interface Props {
-    user: { email: string } | null
+  user: { email: string } | null;
 }
 
+const rents = [
+  {
+    pais: "Argentina",
+    provincia: "Entre rios",
+    ciudad: "Conepcion del Uruguay",
+    direccion: "Rivadavia 610",
+    tipoViviendia: "departamento",
+    precio: "170000",
+    estadoInmueble: "excelente",
+    titulo:
+      " departamento en zona segura, vecinos tranquilos, en excelente estado",
+    clasificacion: "5",
+    idUsuarioPropietario: "1234",
+  },
+  {
+    pais: "argentina",
+    provincia: "Entre rios",
+    ciudad: "Conepcion del Uruguay",
+    direccion: "Rivadavia 610",
+    tipoViviendia: "departamento",
+    precio: "170000",
+    estadoInmueble: "excelente",
+    titulo:
+      " departamento en zona segura, vecinos tranquilos, en excelente estado",
+    clasificacion: "5",
+    idUsuarioPropietario: "1234",
+  },
+  {
+    pais: "argentina",
+    provincia: "Entre rios",
+    ciudad: "Conepcion del Uruguay",
+    direccion: "Rivadavia 610",
+    tipoViviendia: "departamento",
+    precio: "170000",
+    estadoInmueble: "excelente",
+    titulo:
+      " departamento en zona segura, vecinos tranquilos, en excelente estado",
+    clasificacion: "5",
+    idUsuarioPropietario: "1234",
+  },
+];
 
 export default function Rents({ user }: Props) {
-    return (
-        <div className="bg-base-200 pt-10 w-screen h-screen flex justify-center items-start">
-            <div className="flex gap-5 items-center justify-center">
-                <div className='flex-col flex gap-2'>
-                    <h1 className="text-5xl font-bold">Titulo</h1>
-                    <p className=" ">clasificacion</p>
-                    <p className="">Descripcion</p>
-                    <p className="">direcion</p>
-                    <p className="">precio</p>
-                    <button className="btn btn-primary">Mas Informacion</button>
-                </div>
-                <Image src={Logo} alt='image' width={0} height={0} className=" w-64 h-64" />
+  return (
+    <div className="pt-10 w-screen h-full flex flex-col gap-5 justify-center items-center">
+      {rents.map((rent, i) => {
+        return (
+          <div
+            key={i}
+            className="flex gap-5 w-8/12 h-full items-start justify-between bg-base-200 p-10 rounded-xl"
+          >
+            <div className="flex flex-col border-2 gap-5 h-full">
+              <div className="flex-col h-full flex gap-2">
+                <h2 className="text-2xl font-bold">{rent.titulo}</h2>
+                <p className="text-xl">
+                  {rent.pais}, {rent.provincia}, {rent.ciudad}
+                </p>
+                <p className="text-xl ">{rent.direccion}</p>
+                <p className="text-xl">{rent.tipoViviendia}</p>
+                <p className="text-xl">{rent.estadoInmueble}</p>
+              </div>
+              <button className="w-full btn btn-default">
+                Mas Informacion
+              </button>
             </div>
-        </div>
-    )
+            <Image
+              src={Logo}
+              alt="image"
+              width={0}
+              height={0}
+              className=" w-96 h-96"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
